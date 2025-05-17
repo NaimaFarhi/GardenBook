72a13ebae8989ffb4f3429231dd5405885cb0d16
Farhi Naima <naima.farhi01@gmail.com>
2025-05-17 20:15:21 +0100

    fixed the name settings

--- GardenBook/GardenBook/asgi.py
+++ GardenBook/GardenBook/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for GargenBook project.
+ASGI config for GardenBook project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GargenBook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GardenBook.settings')
 
 application = get_asgi_application()
--- GardenBook/GardenBook/wsgi.py
+++ GardenBook/GardenBook/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GargenBook project.
+WSGI config for GardenBook project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GargenBook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GardenBook.settings')
 
 application = get_wsgi_application()
